dfa65f8625a1ecece4c2453ece41ac87d3e12827
ismailcankaratasss@gmail.com <ismailcankaratasss@gmail.com>
2022-01-16 01:37:21 +0300

    localtions added

--- assets/main.js
+++ assets/main.js
@@ -1,21 +1,39 @@
-konumuGetir();
+var localtions = [
+  {businessName: "Cezve Point", latitude: 40.906787, longitude: 29.283012}, //  1,36 km
+  {businessName: "Cezve Point Coffe", latitude: 40.905908, longitude: 29.280641}, //  1,15 km
+  {businessName: "Midyeci Ahmet", latitude: 40.905888, longitude: 29.269996}, //  0,26 km
+  {businessName: "İsmail Can", latitude: 40.9055126, longitude: 29.2669325},
+]
 
-function calcDistance(p1, p2) {
-    var distance = (google.maps.geometry.spherical.computeDistanceBetween(p1, p2) / 1000).toFixed(2);
-    return distance;
+var localDiv = document.getElementById(localtions);
+
+function calcDistance(myLocaltion) {
+  localtions.forEach(local => {
+    var businessLocaltion = new google.maps.LatLng(local.latitude, local.longitude);
+    var distance = (google.maps.geometry.spherical.computeDistanceBetween(myLocaltion, businessLocaltion) / 1000).toFixed(2);
+    document.write(local.businessName + "' a " + distance + " Km <br> <br> ");
+  });
 }
-function konumuGetir() {
+function localtionGet() {
     if (navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition(konumuGoster);
+      navigator.geolocation.getCurrentPosition(localtionShow);
     } else {
       document.write("Tarayıcınız Geolocation API desteklemiyor.");
     }
 }
 
-function konumuGoster(konum) {
-  var p1 = new google.maps.LatLng(konum.coords.latitude, konum.coords.longitude);
-var p2 = new google.maps.LatLng(40.913064443793736, 29.278451732341182);
-    document.write("Konum: " + konum.coords.latitude + ", " + konum.coords.longitude);
-    document.write("<br>Konum2: " + p2);
-    document.write("<br>Mesafe: " + calcDistance(p1, p2) + " Km")
+function localtionShow(location) {
+  var myLocaltion = new google.maps.LatLng(location.coords.latitude, location.coords.longitude);
+  console.log(location.coords.latitude, location.coords.longitude);
+  calcDistance(myLocaltion);
 }
+
+addEventListener('load', ()=> {
+  localtionGet();
+})
+
+
+
+
+
+
